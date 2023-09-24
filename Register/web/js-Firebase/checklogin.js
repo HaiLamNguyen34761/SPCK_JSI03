b4f0b76f92446728f069dbd3c715a726a4939bd8
HaiLamNguyen34761 <nguyenhailam2006.2.9@gmail.com>
2023-09-24 16:26:30 +0700

    init1 SPCK

--- Register/web/js-Firebase/checklogin.js
+++
@@ -1,8 +0,0 @@
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
-import { auth } from "./config.js";
-
-onAuthStateChanged(auth, (user) => {
-  if (!user) {
-    window.location= './login.html'
-  }
-});
